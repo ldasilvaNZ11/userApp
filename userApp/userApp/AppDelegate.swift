cffa4fcfb09ecdbca49d285be9549e037b47a65e
Lucas Da Silva <lucas.dasilva@trademe.co.nz>
2020-06-17 11:26:58 +1200

    App is now programmatically done (no storyboards) // Added tableview to be the RootViewController

---
+++ userApp/userApp/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  userApp
+//
+//  Created by Lucas Da Silva on 17/06/20.
+//  Copyright © 2020 Lucas Da Silva. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    let tableViewController = TableViewController()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        if #available(iOS 13.0, *) {
+            // do nothing here, since we need to set up code in SceneDelegate too
+        }
+        else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = tableViewController
+            window?.makeKeyAndVisible()
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
